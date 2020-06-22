2634d3358dcae7965fef98049b6d4a3da36d2084
giomarc <xkiryax@gmail.com>
2020-06-22 11:56:44 +0200

    pulsar pom for module refactor

---
+++ FlinkAnalyzer/src/main/java/util/KafkaConsumer.java
@@ -0,0 +1,20 @@
+package util;
+
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import java.util.Properties;
+
+public class KafkaConsumer extends FlinkKafkaConsumer<String> {
+
+    public KafkaConsumer() {
+        super("dataQuery1", new SimpleStringSchema(), initProperties());
+    }
+
+    private static Properties initProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "kafka:9092");
+        properties.setProperty("zookeeper.connect", "zookeeper:2181");
+        properties.setProperty("group.id", "test");
+        return properties;
+    }
+}
