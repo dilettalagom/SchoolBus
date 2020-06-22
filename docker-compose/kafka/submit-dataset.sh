4400c3ed211c001183f369e670b827bd4b53b4d8
giomarc <xkiryax@gmail.com>
2020-06-22 12:56:25 +0200

    kafka submit.sh

---
+++ docker-compose/kafka/submit-dataset.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PROGNAME=$0
+
+usage() {
+  cat << EOF >&2
+Usage: $PROGNAME -f <filename> -s <speedfactor>
+-f : filename of dataset to inject
+-s : speed factor for accelerate the ingestion
+     1:   real time
+     10:  10 times faster
+     100: 100 times faster
+EOF
+  exit 1
+}
+
+while getopts f:s:r o; do
+  case $o in
+    (f) filename=$OPTARG;;
+    (s) speedfactor=$OPTARG;;
+    (*) usage
+  esac
+done
+shift "$((OPTIND - 1))"
+echo Remaining arguments: "$@"
+
+echo "filename: "$filename;
+echo "speedfactor: "$speedfactor;
+
+start_ingestion() {
+  java -cp DatasetSenderKafka-1.0-SNAPSHOT.jar Main $filename $speedfactor 1>&1
+}
+start_ingestion
